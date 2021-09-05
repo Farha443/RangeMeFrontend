627f16098771492d97d400ad83bfe2be8b160101
Savas Caliskan <secaliskan@yahoo.com>
2021-09-06 00:08:24 +0300

    Update ServiceProvidersHome.js

--- src/component/service-provider/ServiceProvidersHome.js
+++ src/component/service-provider/ServiceProvidersHome.js
@@ -109,7 +109,7 @@ function onClickButton(e){
                             <div className="supplie-banner-text">
                                 <p className="p1"> anasayfa </p>
                                 <h1> {heading.heading}</h1>
-                                <p className="p2"> {heading.description} <span > Binlerce tedarikçiye hizmetlerini sergile yeni iş fırsatları yaratmanın anahtarına
+                                <p className="p2"> {heading.description} <span > yeni iş fırsatları yaratmanın anahtarına
  </span> </p>
  <NavLink to=""> sahip ol! </NavLink>
                             </div>
@@ -123,7 +123,7 @@ function onClickButton(e){
                     <div className="row">
                         <div className="col-md-12">
                             <div class="hm-slider-title">
-                                <h1>DON'T MISS THE NEW BUSINESS OPPORTUNITIES! <span class="hm-title-h"> KAÇIRMAYIN!  </span> </h1>
+                                <h1>YENİ İŞ FIRSATLARINI<span class="hm-title-h"> KAÇIRMAYIN!  </span> </h1>
                             </div>
                         </div>
                     </div>
@@ -237,7 +237,7 @@ eksik marka kriterleri gibi detayları şeffaf bir şekilde sunar.
                     <div className="row">
                         <div className="col-md-12">
                             <div class="hm-slider-title">
-                                <h1>REACH MORE CUSTOMERS! <span class="hm-title-h"> MÜŞTERİYE ULAŞIN!  </span> </h1>
+                                <h1>DAHA FAZLA<span class="hm-title-h"> MÜŞTERİYE ULAŞIN!  </span> </h1>
                             </div>
                         </div>
                         </div>
@@ -294,7 +294,7 @@ eksik marka kriterleri gibi detayları şeffaf bir şekilde sunar.
                     <div className="row">
                         <div className="col-md-12 col-xs-12">
                             <div className="hm-slider-title">
-                            <h1>base <span class="hm-title-h"> Alıcılar </span> </h1>
+                            <h1>BAZI<span class="hm-title-h"> TEDARİKÇİLER </span> </h1>
                             </div>
                         </div>
 
