2fdb0c32679d257261493e1873c915c777b29977
Savas Caliskan <secaliskan@yahoo.com>
2021-09-08 12:27:49 +0300

    Update SignupModal.js

--- src/component/element/SignupModal.js
+++ src/component/element/SignupModal.js
@@ -67,7 +67,7 @@ function Func() {
 
   if (typeof password !== "undefined" && typeof confirm_password !== "undefined") {
     if (password!= confirm_password) {
-      alert("Passwords do not match.")
+      alert("Girilen şifreler aynı değil")
     }
 } 
 
@@ -75,12 +75,12 @@ function Func() {
     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
   
     if (!pattern.test(email)) {
-        alert("Enter a valid email")
+        alert("Geçerli bir eposta adresi giriniz")
     }
   
   }
   if(terms==null){
-    alert("Please agree to the terms and conditions first. ");
+    alert("Lütfen ilk önce Kullanım Koşulları ve Gizlilik Politikasını kabul edin. ");
   }
   else{
   axios.post( BASE_URL + "authentication/signup/",
@@ -95,13 +95,13 @@ function Func() {
 
       }).then(res=>{
         if (res.data.message === "Email id is required"){
-          alert("Email id is required");
+          alert("eposta zorunlu bir alandır");
         }
         else if (res.data.message === "Email id already exists"){
-          alert("Email id already exists");
+          alert("Bu eposta adresi ile bir hesap, sitemizde zaten mevcut");
         }
         else if (res.data.message === "Password is required"){
-          alert("Password is required");
+          alert("Şifre girişi zorunlu bir alandır");
         }
         document.getElementById('s2').className='st-two';
         document.getElementById('s1').className='st-one d-none'
@@ -163,7 +163,7 @@ axios(config).then(res=>{
 
 ).catch(err=>{
 document.getElementById('s2').className='st-two';
-alert("something went wrong");
+alert("Üzgünüz, bir şeyler ters gitti");
 })
 
 }
@@ -196,7 +196,7 @@ function brandSubmit(){
 
       Swal.fire({
         title: 'Kayıt İşleminiz Başarılı Oldu!',
-        text: 'Giriş ekranına yönlendiriliyorsunuz...',
+        text: 'Giriş sayfasına yönlendiriliyorsunuz...',
         timerProgressBar: true,
         type: 'success',
         timer: 3000,
@@ -213,7 +213,7 @@ function brandSubmit(){
       console.error(err);
       // $(".laoder").hide(); 
       document.getElementById('s3').className='st-three'
-    alert("something went wrong")
+    alert("Üzgünüz, bir şeyler ters gitti")
     })
   // }else{
   //   alert("Please Enter a valid URL");
@@ -459,7 +459,7 @@ function BuyerStep2() {
   
     axios(config).then(res=>{
       Swal.fire({
-        title: 'Kayıt İşlemi Başarılı Oldu!',
+        title: 'Kayıt İşleminiz Başarılı Oldu!',
         text: 'Giriş sayfasına yönlendiriliyorsunuz...',
         timerProgressBar: true,
         type: 'success',
@@ -649,8 +649,8 @@ axios(config).then(res=>{
               <Tabs>
                 <TabList>
                   <Tab onClick={click}> <div  className="sign-tab-menu"> <i class="fas fa-user-tie"></i> Tedarikçi </div> </Tab>
-                  <Tab onClick={Click1}> <div className="sign-tab-menu"> <i class="fas fa-store"></i> Alıcı</div> </Tab>
-                  <Tab onClick={Click2}> <div className="sign-tab-menu"> <i class="fas fa-cog"></i> Hizmet Sağlayıcı</div> </Tab>
+                  <Tab onClick={Click1}> <div className="sign-tab-menu"> <i class="fas fa-store"></i> Alıcı </div> </Tab>
+                  <Tab onClick={Click2}> <div className="sign-tab-menu"> <i class="fas fa-cog"></i> Hizmet Sağlayıcı </div> </Tab>
                 </TabList>
 
                 <TabPanel>
@@ -720,8 +720,8 @@ axios(config).then(res=>{
                         </Col>
                         <Col md="12" xs="12">
                           <Form.Group className="signup-f-group" controlId="formBasicEmail" >
-                            <Form.Label>E-mail Adresiniz</Form.Label>
-                            <Form.Control type="email"   id="email" placeholder="Lütfen eposta adresinizi giriniz..." />
+                            <Form.Label>E-posta Adresiniz</Form.Label>
+                            <Form.Control type="email"   id="email" placeholder="Lütfen e-posta adresinizi giriniz..." />
                           </Form.Group>
                         </Col>
                         <Col md="12" xs="12">
@@ -957,8 +957,8 @@ axios(config).then(res=>{
 
                         <Col md="6" xs="12">
                           <Form.Group className="signup-f-group" controlId="formBasicEmail" >
-                            <Form.Label>Web Sitesi Adresi</Form.Label>
-                            <Form.Control type="text" id="brand_website" placeholder="Lütfen websitenizin adresinizi (www...) giriniz..." />
+                            <Form.Label>Web Siteniz </Form.Label>
+                            <Form.Control type="text" id="brand_website" placeholder="Lütfen websitenizi giriniz (örnek www.abc.com)" />
                           </Form.Group>
                         </Col>
 
@@ -1614,7 +1614,7 @@ axios(config).then(res=>{
                                             <p> 3 </p>
                                           </div>
                                           <div className="step-text">
-                                            <p> <span>MARKA</span> BİLGİLER </p>
+                                            <p> <span>MARKA</span> BİLGİLERİ </p>
                                           </div>
                                         </li>
                                       </ul>
@@ -1656,21 +1656,21 @@ axios(config).then(res=>{
 
                         <Col md="12" >
                               <div className="mb-3 sign-radio">
-                              <p>what area can you service ? </p>
+                              <p>Nerede Hizmet Sağlıyorsunuz? </p>
                                 <Form.Check
                                   inline
-                                  label="National"
+                                  label="Ülke Bazında"
                                   name="group1"
                                   type="radio"
-                                  value="National" 
+                                  value="Ülke Bazında" 
                                   id="1"
                                   onClick={servicechange}
                                 />
                                 <Form.Check
                                   inline
-                                  label="State"
+                                  label="Şehir Bazında"
                                   name="group1"
-                                  value="true"
+                                  value="Şehir Bazında"
                                   type="radio"
                                   id="2"
                                   onClick={servicechange}
@@ -1678,8 +1678,8 @@ axios(config).then(res=>{
 
                                  <Form.Check
                                   inline
-                                  label="Global"
-                                  value="Global"
+                                  label="Tüm Ülkeler Bazında"
+                                  value="Tüm Ülkeler Bazında"
                                   name="group1"
                                   type="radio"
                                   id="2"
